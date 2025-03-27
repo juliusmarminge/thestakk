7a821e9b615f49e1fd6b646d0bd87e937eb24cd1
juliusmarminge <jmarminge@gmail.com>
2025-03-28 00:35:46 +0100

    only apply grayscale in mono theme

--- app/components/nav-user.tsx
+++ app/components/nav-user.tsx
@@ -56,7 +56,7 @@ export function NavUser() {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
+              <Avatar className="h-8 w-8 rounded-lg [.theme-mono_&]:grayscale">
                 <AvatarImage
                   src={session.user.image ?? "https://github.com/shadcn.png"}
                   alt={session.user.name}
