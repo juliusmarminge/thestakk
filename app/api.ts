a3a3abf798fdbb9ff288a394a5969bf264216356
juliusmarminge <jmarminge@gmail.com>
2025-05-11 21:15:39 +0200

    nit

--- app/api.ts
+++ app/api.ts
@@ -1,7 +1,3 @@
 import { createStartAPIHandler, defaultAPIFileRouteHandler } from "@tanstack/react-start/api";
 
-export default createStartAPIHandler((event) => {
-  const url = new URL(event.request.url);
-  console.log("[api] url", url.href);
-  return defaultAPIFileRouteHandler(event);
-});
+export default createStartAPIHandler(defaultAPIFileRouteHandler);
