c50a90c169f8d60e451e4435b563743693ab10c3
Julius <julius0216@outlook.com>
2025-11-02 20:16:46 -0800

    nicee

--- src/auth/client.ts
+++ src/auth/client.ts
@@ -1,10 +1,26 @@
 import { convexClient } from "@convex-dev/better-auth/client/plugins";
 import { queryOptions } from "@tanstack/react-query";
+import { createIsomorphicFn } from "@tanstack/react-start";
+import { getCookie, getRequest } from "@tanstack/react-start/server";
 import { passkeyClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
+export const getConvexToken = createIsomorphicFn()
+  .server(() => getCookie("better-auth.convex_jwt"))
+  .client(() => undefined);
+
+export const getRequestCookie = createIsomorphicFn()
+  .server(() => getRequest().headers.get("Cookie"))
+  .client(() => undefined); // browser handles this
+
 export const authClient = createAuthClient({
   plugins: [passkeyClient(), convexClient()],
+  fetchOptions: {
+    onRequest(context) {
+      const cookie = getRequestCookie();
+      if (cookie) context.headers.set("cookie", cookie);
+    },
+  },
 });
 
 export const sessionQueryOptions = queryOptions({
@@ -20,7 +36,11 @@ export const passkeysQueryOptions = queryOptions({
   queryKey: ["passkeys"],
   queryFn: async () => {
     const passkeys = await authClient.passkey.listUserPasskeys();
+    console.log("passkeys", passkeys);
     if (passkeys.error) throw passkeys.error;
-    return passkeys.data;
+    return passkeys.data.map((passkey) => ({
+      ...passkey,
+      createdAt: new Date(passkey.createdAt),
+    }));
   },
 });
